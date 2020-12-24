076f1999690c547d88494bd6ab3287dcc9435db1
DutChen18 <dutchen18@gmail.com>
2020-12-24 19:03:32 +0100

    Add simplex noise animation

--- xmaslights-spin.py
+++ xmaslights-spin.py
@@ -9,6 +9,7 @@ def xmaslight():
     import neopixel
     import re
     import math
+    import opensimplex
     
     # You are welcome to add any of these:
     # import random
@@ -45,47 +46,23 @@ def xmaslight():
     
     # YOU CAN EDIT FROM HERE DOWN
     
-    # I get a list of the heights which is not overly useful here other than to set the max and min altitudes
-    heights = []
-    for i in coords:
-        heights.append(i[2])
-    
-    min_alt = min(heights)
-    max_alt = max(heights)
-    
     # VARIOUS SETTINGS
-    
-    # how much the rotation points moves each time
-    dinc = 1
-    
-    # a buffer so it does not hit to extreme top or bottom of the tree
-    buffer = 200
     
     # pause between cycles (normally zero as it is already quite slow)
     slow = 0
-    
-    # startin angle (in radians)
-    angle = 0
-    
-    # how much the angle changes per cycle
-    inc = 0.1
-    
-    # the two colours in GRB order
-    # if you are turning a lot of them on at once, keep their brightness down please
-    colourA = [0,50,50] # purple
-    colourB = [50,50,0] # yellow
-    
-    
-    # INITIALISE SOME VALUES
-    
-    swap01 = 0
-    swap02 = 0
-    
-    # direct it move in
-    direction = -1
-    
-    # the starting point on the vertical axis
-    c = 100 
+
+    # simplex
+    simplex_r = opensimplex.OpenSimplex(0)
+    simplex_g = opensimplex.OpenSimplex(1)
+    simplex_b = opensimplex.OpenSimplex(2)
+
+    # brightness
+    brightness = 100
+
+    # position
+    pos = (0.0, 0.0, 0.0)
+    delta = (0.0, 0.0, 25.0)
+    zoom = 400
     
     # yes, I just run which run is true
     run = 1
@@ -95,48 +72,26 @@ def xmaslight():
         
         LED = 0
         while LED < len(coords):
-            if math.tan(angle)*coords[LED][1] <= coords[LED][2]+c:
-                pixels[LED] = colourA
-            else:
-                pixels[LED] = colourB
+            x = (coords[LED][0] + pos[0]) / zoom
+            y = (coords[LED][1] + pos[1]) / zoom
+            z = (coords[LED][2] + pos[2]) / zoom
+            r = (simplex_r.noise3d(x, y, z) + 1) * brightness / 2
+            g = (simplex_g.noise3d(x, y, z) + 1) * brightness / 2
+            b = (simplex_b.noise3d(x, y, z) + 1) * brightness / 2
+            pixels[LED] = [g, r, b]
             LED += 1
         
         # use the show() option as rarely as possible as it takes ages
         # do not use show() each time you change a LED but rather wait until you have changed them all
         pixels.show()
-        
-        # now we get ready for the next cycle
-        
-        angle += inc
-        if angle > 2*math.pi:
-            angle -= 2*math.pi
-            swap01 = 0
-            swap02 = 0
-        
-        # this is all to keep track of which colour is 'on top'
-        
-        if angle >= 0.5*math.pi:
-            if swap01 == 0:
-                colour_hold = [i for i in colourA]
-                colourA =[i for i in colourB]
-                colourB = [i for i in colour_hold]
-                swap01 = 1
-                
-        if angle >= 1.5*math.pi:
-            if swap02 == 0:
-                colour_hold = [i for i in colourA]
-                colourA =[i for i in colourB]
-                colourB = [i for i in colour_hold]
-                swap02 = 1
-        
-        # and we move the rotation point
-        c += direction*dinc
-        
-        if c <= min_alt+buffer:
-            direction = 1
-        if c >= max_alt-buffer:
-            direction = -1
-        
+
+        # update position
+        pos = (
+            pos[0] + delta[0],
+            pos[1] + delta[1],
+            pos[2] + delta[2],
+        )
+
     return 'DONE'
 
 
